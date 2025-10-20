7643fbbe92d6724b590f2c1cbb9c539cfc4b7b5a
John-Kuenane <mjohnkuenane@gmail.com>
2025-10-20 14:59:34 +0200

    Feat:nissa_data_flow_

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsl(214.3 31.8% 91.4%)",
+        ring: "hsl(215 20.2% 65%)",
+      },
+    },
   },
   plugins: [],
 };
